c97def09ab05c4b63f9563d91fe1c3c5fdc8b1f2
Göksel Sina <48367016+gokselsina@users.noreply.github.com>
2020-06-30 15:14:38 +0300

    Add files via upload

---
+++ sudoku/sudoku/Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace sudoku
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(430, 44);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(204, 43);
+            this.button1.TabIndex = 78;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Transparent;
+            this.button2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button2.BackgroundImage")));
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button2.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Location = new System.Drawing.Point(430, 93);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(204, 43);
+            this.button2.TabIndex = 79;
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Transparent;
+            this.button3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button3.BackgroundImage")));
+            this.button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button3.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Location = new System.Drawing.Point(430, 263);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(204, 43);
+            this.button3.TabIndex = 80;
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Transparent;
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Controls.Add(this.button8);
+            this.panel1.Controls.Add(this.button7);
+            this.panel1.Controls.Add(this.button6);
+            this.panel1.Controls.Add(this.pictureBox2);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(678, 334);
+            this.panel1.TabIndex = 83;
+            // 
+            // button8
+            // 
+            this.button8.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button8.BackgroundImage")));
+            this.button8.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button8.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.button8.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button8.Location = new System.Drawing.Point(237, 234);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(204, 43);
+            this.button8.TabIndex = 4;
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button7
+            // 
+            this.button7.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button7.BackgroundImage")));
+            this.button7.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button7.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.button7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button7.Location = new System.Drawing.Point(237, 185);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(204, 43);
+            this.button7.TabIndex = 3;
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button6
+            // 
+            this.button6.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button6.BackgroundImage")));
+            this.button6.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button6.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button6.Location = new System.Drawing.Point(237, 136);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(204, 43);
+            this.button6.TabIndex = 2;
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox2.BackgroundImage")));
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox2.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(672, 37);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox1.Location = new System.Drawing.Point(3, 42);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(672, 79);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button4
+            // 
+            this.button4.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button4.BackgroundImage")));
+            this.button4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button4.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Location = new System.Drawing.Point(237, 283);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(204, 43);
+            this.button4.TabIndex = 5;
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(702, 361);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "SUDOKU - GP3 Ödev";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ sudoku/sudoku/Form1.cs
@@ -0,0 +1,356 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace sudoku
+{
+    public partial class Form1 : Form
+    {
+
+        TextBox[,] sudo = new TextBox[9, 9];
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            olustur();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            yeniOyun();
+        }
+
+        public void olustur()
+        {           
+            int locationa = 30;
+            int locationb = 30;
+            int sayaca = 0;
+            int sayacb = 0;
+            Font tfont = button1.Font;
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    sudo[i, j] = new TextBox();
+                    sudo[i, j].TextChanged += new EventHandler(giris);
+                    Controls.Add(sudo[i, j]);
+                    sudo[i, j].Width = 30;
+                    sudo[i, j].Font = new Font(tfont.FontFamily, 15);
+                    sudo[i, j].MaxLength = 1;
+                    sudo[i, j].Location = new Point(locationa, locationb);
+                    sudo[i, j].TextAlign = HorizontalAlignment.Center;
+                    sayaca++;
+                    locationa += 32;       
+                    if (sayaca == 3) { sayaca = 0; locationa += 5; }
+                }
+                locationa = 30;
+                locationb += 32;
+                sayacb++;
+                if (sayacb == 3) { sayacb = 0; locationb += 5; }
+            }
+            locationb = 30;
+        }
+
+        public void yeniOyun()
+        {
+            sifirla();
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    sudo[i, j].Text = "";
+                    sudo[i, j].Enabled = true;
+                    sudo[i, j].BackColor = default;
+                }
+            }
+
+            string dizi = ""; // sonuç stringi.
+            bool sil = false; // ilk satır silmek için kontrol değişkeni
+            String URLString = " https://www.noordpress.com/sudoku.php"; // xml link
+            XmlTextReader reader = new XmlTextReader(URLString); // xml okumak için reader tanımladık.
+            while (reader.Read()) // okuma işlemi
+            {
+                dizi += reader.Value.Replace(",", string.Empty); // veriyi dizi adlı string içinde birleştir.
+                if (sil == false) { sil = true; dizi = ""; } // ilk satırla gelen sürüm bilgilerini dahil etme.
+            }
+            int sayac = 0; // hücrelere numaraları aktarırken kullanacağımız parametre değişkeni.
+            for (int i = 0; i < 9; i++) // 9x9 hücre için for döngüsü
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    if (dizi.Substring(sayac, 1) != "0") // eğer numara 0 ise hücreyi boş bırak.
+                    {
+                        sudo[i, j].Text = dizi.Substring(sayac, 1); 
+                        sudo[i, j].Enabled = false; // doldurulan hücreyi düzenlemeyi kapat
+                    }
+                    sayac++; // sayacı arttır.
+                }
+            }
+            reader.Close();
+        }
+
+        public bool kontrol()
+        {
+            for (int i = 0; i < 9; i++) // her satır için tekrar kullanım kontrolü
+            {
+                int[] ctrl = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }; 
+                for (int j = 0; j < 9; j++)
+                {
+                    if (sudo[i, j].Text != "") 
+                    {                   
+                    ctrl[Convert.ToInt32(sudo[i, j].Text)]++;
+                    // tekrar kullanımda false döndürür
+                    if (ctrl.Contains(2) == true) { return (false); } 
+                    }
+                }
+            }
+
+            for (int i = 0; i < 9; i++) // her sütun için tekrar kullanım kontrolü
+            {
+                int[] ctrl = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+                for (int j = 0; j < 9; j++)
+                {
+                    if (sudo[j, i].Text != "")
+                    {
+                        ctrl[Convert.ToInt32(sudo[j, i].Text)]++;
+                        // tekrar kullanımda false döndürür
+                        if (ctrl.Contains(2) == true) { return (false); }
+                    }
+                }
+            }
+
+            // her 3x3 hücre grubu için döngü
+            for (int y = 0; y < 7; y += 3)
+            {
+                for (int x = 0; x < 7; x += 3)
+                {
+                    int[] ctrl = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+                    for (int i = 0; i < 3; i++)
+                    {
+                        for (int j = 0; j < 3; j++)
+                        {
+                            if (sudo[(i + y), (j + x)].Text != "")
+                            {
+                                ctrl[Convert.ToInt32(sudo[(i + y), (j + x)].Text)]++;
+                                // tekrar kullanımda false döndürür
+                                if (ctrl.Contains(2) == true) { return (false); }
+                            }
+                        }
+                    }
+                }
+            }
+            // hatalara takılmaz ise true döndür
+            return (true);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            kaydet();         
+        }
+
+        public void kaydet()
+        {
+            SaveFileDialog save = new SaveFileDialog();
+            save.Filter = "XML Dosyası|*.xml";
+            save.OverwritePrompt = true;
+            save.CreatePrompt = true;
+            save.Title = "İlerlemeni Kaydet";
+
+            if (save.ShowDialog() == DialogResult.OK)
+            {
+                string text = "";
+                XmlWriter xmlYazici = XmlWriter.Create(save.FileName);
+
+                xmlYazici.WriteStartDocument();
+                xmlYazici.WriteStartElement("puzzle");
+                xmlYazici.WriteStartElement("rows");
+                for (int i = 0; i < 9; i++)
+                {
+                    xmlYazici.WriteStartElement("row");
+                    for (int j = 0; j < 9; j++)
+                    {
+                        if (sudo[i, j].Text == "") {
+                            text += "0";
+                        }
+                        else
+                        {
+                            text += sudo[i, j].Text;
+                            if (j != 8) { text += ","; }
+                        }
+                    }
+                    xmlYazici.WriteString(text);
+                    xmlYazici.WriteEndElement();
+                    text = "";
+                }
+                xmlYazici.WriteEndElement();
+                xmlYazici.WriteEndElement();
+                xmlYazici.WriteEndDocument();
+                xmlYazici.Close();
+            }           
+        }
+
+        public void kayitac()
+        {          
+            OpenFileDialog file = new OpenFileDialog();
+            file.RestoreDirectory = true;
+            file.Filter = "XML Dosyası|*.xml";
+            file.Title = "Kayıtlı Oyunun XML Dosyasını Seçin.";
+
+            if (file.ShowDialog() == DialogResult.OK)
+            {
+                string DosyaYolu = file.FileName;
+
+                sifirla();
+                for (int i = 0; i < 9; i++)
+                {
+                    for (int j = 0; j < 9; j++)
+                    {
+                        sudo[i, j].Text = "";
+                        sudo[i, j].Enabled = true;
+                    }
+                }
+
+                string dizi = "";
+                bool sil = false;
+                String URLString = file.FileName;
+                XmlTextReader reader = new XmlTextReader(URLString);
+                while (reader.Read())
+                {
+                    dizi += reader.Value.Replace(",", string.Empty);
+                    if (sil == false) { sil = true; dizi = ""; } // ilk satırla gelen sürüm bilgilerini sildik.
+                }
+                int sayac = 0;
+                for (int i = 0; i < 9; i++)
+                {
+                    for (int j = 0; j < 9; j++)
+                    {
+                        if (dizi.Substring(sayac, 1) != "0")
+                        {
+                            sudo[i, j].Text = dizi.Substring(sayac, 1);
+                            sudo[i, j].Enabled = false;
+                        }
+                        sayac++;
+                    }
+                }
+                reader.Close();
+                panel1.Visible = false;
+            }
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("EGE ÜNİVERSİTESİ\nTire Kutsan Meslek Yüksekokulu\nBilgisayar Programcılığı\nGöksel Sina BİLECEN\n60180000018");
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            panel1.Visible = false;
+            yeniOyun();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            DialogResult cikis = new DialogResult();
+            cikis = MessageBox.Show("İlerlemenizi kaydetmediyseniz silinecek. Devam etmek istiyormusunuz ?", "Ana Menü", MessageBoxButtons.YesNo);
+            if (cikis == DialogResult.Yes)
+            {
+                sifirla();
+                panel1.Visible = true;
+            }
+        }
+        protected void giris(object sender, EventArgs e)
+        {
+            int ti=0, tj=0; // mevcut textBox parametrelerini tutmak için değişken oluşturduk.
+            for (int i = 0; i < 9; i++) // 9x9 hücre döngüsü
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    if (sudo[i, j].Focused) { ti = i; tj = j; break; } // odaklanmış ise parametreyi değişkenlere al
+                }                 
+            }
+            
+            // kullanıcının sayı dışında veri girmesine karşı..
+            long val; // long değişşkeni oluşturduk
+            try{
+                // girilen değer boş değilse Int64 çevirme işlemi yap
+                if (sudo[ti, tj].Text != "") { val = System.Convert.ToInt64(sudo[ti, tj].Text); } 
+            }
+            catch{ // hata veriyorsa sayı değildir
+                MessageBox.Show("Sadece rakam girebilirsiniz."); // gerekli uyarıyı ver
+                sudo[ti, tj].Clear(); // mevcut textBox verisini temizle
+            }
+
+            if (!kontrol()) { // kontrol metodu false değer döndürürse yani tekrar kullanım yapılmış ise
+                MessageBox.Show("Girdiğiniz rakam bu hücre içerisinde tekrar kullanılamaz."); // uyarı ver
+                sudo[ti, tj].Clear(); // mevcut textBox verisini temizle
+            }
+
+            // kullanıcı 0 girerse
+            if (sudo[ti, tj].Text == "0") { MessageBox.Show("Yalnız 1'den 9'a kadar olan rakamları kullanabilirsiniz."); sudo[ti, tj].Clear(); }
+
+            // tüm hücreler hatalara takılmadan doldurulmuş ise oyunBitti metodunu çalıştır 
+            if (doluMu()) { MessageBox.Show("Tebrikler. Oyunu Kazandınız !"); oyunBitti(); }
+        }
+
+        public bool doluMu()
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    // textBox dolu mu kontrolü. 
+                    if (sudo[i, j].Text == "") { return (false); }
+                }
+            }
+
+            // tüm hücreler dolu ise true döndür
+            return (true);
+        }
+
+        public void oyunBitti()
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    // oyun kazanıldıysa tüm hücreler için düzenlemeyi kapat
+                    // tüm hücreleri yeşile boya :)
+                    sudo[i, j].Enabled = false;
+                    sudo[i, j].BackColor = Color.Green;
+                }
+            }
+        }
+
+        public void sifirla() // tüm hücreleri temizler
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    sudo[i, j].Clear();
+                }
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            kayitac();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
